49ac77de044c1a063d03d65ae1ad9b361a64c9a1
Arham Jawed <arham0jawed@gmail.com>
2024-06-09 00:39:02 +0530

    adding second portion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import FeatureSection from "@/components/FeatureSection";
 import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] ">
+         <div className="h-screen">
          <HeroSection />
+         </div>
+         <FeatureSection />
     </main>
   );
 }
---
+++ src/components/FeatureSection.tsx
@@ -0,0 +1,62 @@
+"use client";
+import courseData from "@/data/music_courses.json";
+import Link from "next/link";
+import { BackgroundGradient } from "./ui/background-gradient";
+interface Course {
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  price: number;
+  instructor: string;
+  isFeatured: boolean;
+}
+
+function FeatureSection() {
+  const featuredCourses = courseData.courses.filter(
+    (course: Course) => course.isFeatured
+  );
+  return (
+    <div className="py-12 bg-gray-900">
+      <div>
+        <div className="text-center">
+          <h2 className="text-2xl text-teal-600 font-normal tracking-wide uppercase">
+            FEATURED COURSES
+          </h2>
+          <h1 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
+            Learn With The Best
+          </h1>
+        </div>
+      </div>
+      <div className="mt-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
+          {featuredCourses.map((course: Course) => (
+            <div className="flex justify-center ">
+              <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
+                <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
+                  <p className="sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200 text-2xl font-semibold">
+                    {course.title}
+                  </p>
+                  <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">
+                    {course.description}
+                  </p>
+                  <Link className="mt-6" href={`/courses/${course.slug}`}>Learn More</Link>
+                </div>
+              </BackgroundGradient>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="mt-20 text-center">
+        <Link
+          href={"/courses"}
+          className="bg-white p-3 rounded border-black text-black hover:bg-black hover:text-white transition duration-150 ease-out hover:ease-in"
+        >
+          View All Courses
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default FeatureSection;
